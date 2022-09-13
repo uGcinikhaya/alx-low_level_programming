5e70b74cbe36284b36bc2fc6d5ce2335925124be
uGcinikhaya <gcinikhayamthethwa@gmail.com>
2022-09-12 19:26:37 -0700

    Print AlphabetIn Lower Case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints letters of the alphabet
+ * in lower case.
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char letter = a;
+
+	while (letter <= z)
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,3 @@
 A program that will assign a random number to the variable n each time it is executed.
 A program that will assign a random number to the variable n each time it is executed.
+A program that prints the alphabet in lowercase.
