add64a3c54ef263f378475007adc56ee6a35fc02
uGcinikhaya <gcinikhayamthethwa@gmail.com>
2022-09-14 10:18:38 -0700

    islowe

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - returns 1 if char is lowercase
+ *
+ * @c: character to be checked if lower case, type int
+ * c is an ascii character.
+ *
+ * Return: 1 if lowercase, 0 if otherwise.
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,5 @@
 A program that prints _putchar.
 A function that prints the alphabet, in lowercase.
 A function that prints 10 times the alphabet, in lowercase.
+A function that checks for lowercase character.
+A function that checks for lowercase character.
