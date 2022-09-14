65aff985e92195433eb18f308e8e50a642d1e685
uGcinikhaya <gcinikhayamthethwa@gmail.com>
2022-09-14 03:04:55 -0700

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints alphabet in lower case.
+ *
+ * Return: 0 means Success.
+ */
+
+void print_alphabet(void);
+{
+	char E;
+
+	for (E = 'a'; E <= 'z'; E++)
+	{
+		_putchar(E);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,2 @@
 A program that prints _putchar.
+A unction that prints the alphabet, in lowercase.
