689a495ce8113c939df30f9dfe1e7731471a019d
uGcinikhaya <gcinikhayamthethwa@gmail.com>
2022-09-14 20:27:53 -0700

    The world looks

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the times table
+ * @g: integer for which the times table will print in
+ * Description: prints the times table
+ * Return: nothing.
+ */
+
+void print_times_table(int g)
+{
+	int row, col;
+	int mult = 0;
+
+	if (g >= 0 && g <= 15)
+	{
+		for (row = 0; row <= g; row++)
+		{
+			mult = (row * col);
+			if (mult < 10)
+			{
+				if (col != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(mult + '0');
+			}
+			else if (mult < 100)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((mult / 10) + '0');
+				_putchar((mult % 10) + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar((mult / 100) + '0');
+					_putchar(((mult % 100) / 10) + '0');
+					_putchar((mult % 10) + '0');
+				}
+				if (col != g)
+					_putchar(',');
+			}
+			_putchar('\n');
+		}
+	}
+}
+
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -11,3 +11,4 @@ A function that prints every minute of the day of Jack Bauer, starting from 00:0
 A function that prints the 9 times table, starting with 0.
 A function that adds two integers and returns the result.
 A function that prints all natural numbers from n to 98.
+A function that prints the n times table, starting with 0.
