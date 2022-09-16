7bbbd8dec506039381aa0493c97d7010395b59d5
uGcinikhaya <gcinikhayamthethwa@gmail.com>
2022-09-15 18:59:00 -0700

    low level programming

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,38 +3,11 @@
 /**
  * main - Entry point
  *
- * Description: Print double digit combos 00-99
+ * Description: Print double digit combinations
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int i, j;
-
-	i = 48;
-	j = 48;
-
-	while (i < 58)
-	{
-		j = i = 1;
-		while (j < 58)
-		{
-			putchar(i);
-			putchar(j);
-
-			if (i < 56 || j < 57)
-			{
-				putchar(44);
-				putchar(32);
-			}
-			j++;
-		}
-
-		i++;
-	}
-
-	putchar(10);
-
-	return (0);
-}
+	/*
