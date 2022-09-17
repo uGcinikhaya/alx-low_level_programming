399ba274b4ace57162fc64df4778f78ac958a194
uGcinikhaya <gcinikhayamthethwa@gmail.com>
2022-09-16 20:41:27 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - FizzBuzz Interview Test
+ *
+ * Description: print numbers 1 - 100, replacing multiples of
+ * 3 with Fizz and multiples of 5 with Buzz and multiples of
+ * both with FizzBuzz
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	for (int i = 1; i < 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("Fizz Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	return (0);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -6,4 +6,5 @@ A function that prints the numbers from 0 -9.
 A function that prints 10 times the numbers from 0 - 14
 A function that draws a straight line in the terminal.
 A function that draws a diagonal line in the terminal.
-A function that prints a square
+A function that prints a square.
+A program that prints the numbers 1 to 100.
