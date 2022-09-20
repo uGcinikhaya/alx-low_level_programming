b67a840509ec53bb8fc1bdd4d2543667bcf3f69b
uGcinikhaya <gcinikhayamthethwa@gmail.com>
2022-09-20 01:22:18 -0700

    i am not afraid of computers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,6 +3,7 @@
 /**
  * _puts - prints a string.
  * @str: input string to print
+ * Description: prrints a string
  * Return: nothing
  */
 
@@ -10,9 +11,9 @@ void _puts(char *str)
 {
 	int i = 0;
 
-	while (*(str = i) != '\0')
+	while (*(str + i) != '\0')
 	{
-		putchar(*(str + 1));
+		putchar(*(str + i));
 		i++;
 	}
 
