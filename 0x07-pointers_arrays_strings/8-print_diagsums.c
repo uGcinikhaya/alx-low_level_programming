e1665ad63bff5a45164bea75fb3ea01dd47a4f38
uGcinikhaya <gcinikhayamthethwa@gmail.com>
2022-09-27 22:07:42 -0700

    line of life

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - prints the sum of two diagonals of a square matrix
+ * print_diagsums - prints the sum of two diagonals
+ * of a square matrix
  * @a: pointer to start a matrix
  * @size: width of matrix column
  *
@@ -16,7 +17,7 @@ void print_diagsums(int *a, int size)
 	for (i = 0; i < size; i++)
 	{
 		p = (i * size) + 1;
-		l += *(a +p);
+		l += *(a + p);
 	}
 	for (j = 0; j < size; j++)
 	{
