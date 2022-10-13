82bbbe6976272bb8f0a77c4919a0acb7bd2bbb7e
uGcinikhaya <gcinikhayamthethwa@gmail.com>
2022-10-12 22:05:41 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,7 +9,7 @@
  * Return: pointer to newly allocated memory or NULL if fails
  */
 
-int *array_rangle(int min, int max)
+int *array_range(int min, int max)
 {
 	int i, j;
 	int *g;
@@ -17,7 +17,7 @@ int *array_rangle(int min, int max)
 	if (min > max)
 		return (NULL);
 	j = max - min + 1;
-	g = malloc(sizeof(int) * j);
+	g = malloc(sizeof(int) * l);
 	if (g == NULL)
 		return (NULL);
 	for (i = 0; i < j; i++, min++)
